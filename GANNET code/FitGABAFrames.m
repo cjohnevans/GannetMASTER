dca4cc8d737797dc64ea5c47d921c1f99088d8f4
cjohnevans <cje.cubric@gmail.com>
2012-05-10 17:20:47 +0100

    Working, more or less

--- GANNET code/FitGABAFrames.m
+++ GANNET code/FitGABAFrames.m
@@ -7,14 +7,11 @@ function [MRS_struct] = FitGABAFrames(MRS_struct,ii)
 % ii = index of spectrum to analyse in MRS_struct
 
 % From GannetFit 120510
-
-% hello from daws
-
 FIT_LSQCURV = 0;
 FIT_NLINFIT = 1;
 
 fit_method = FIT_NLINFIT; %FIT_NLINFIT;
-waterfit_method = FIT_NLINFIT;
+
 
 % Needs to be run on GABAerror branch of Gannet - i.e. needs
 %    diffSubSpectrum array.
@@ -27,19 +24,43 @@ freq=MRS_struct.freq;
 % if strcmp(MRS_struct.Reference_compound,'H2O')
 %     WaterData=MRS_struct.waterspec;
 % end
+
 MRS_struct.versionfit = '120222a';
 disp(['GABA Fit Version is ' MRS_struct.versionfit ]);
-fitwater=1;
 numscans=size(GABAData);
 numscans=numscans(1);
 
-%110624
-epsdirname = [ 'MRSfit_' datestr(clock,'yymmdd') ];
-numsubspec = MRS_struct.Navg(ii)/4; % Navg / (Nphasecycles * N_ON_OFF)
+size(GABAData(:,1))
+% remove blank lines
+for kk = numscans:-1:1
+    if (GABAData(kk,1) == 0)
+        GABAData(kk,:) = [];
+    end
+end
 
-for jj = 1:numsubspec
-    
-    
+numsubspec = length(GABAData(:,1));
+
+% For SNR, average subspectra in blocks of 8
+subspecblock = 8;
+npoints = length(GABAData(1,:));
+nblocks = fix( numsubspec / subspecblock );
+nremain = mod( numsubspec , subspecblock );
+
+
+blockGABAdata = GABAData(1:(end-nremain),:); % discard remainder, until I think of a better way... 
+blockGABAdata = blockGABAdata'; % to [points, subspectra]
+size(blockGABAdata)
+
+blockGABAdata = reshape(blockGABAdata, [npoints, subspecblock, nblocks]);
+% sum across blocks
+blockGABAdata = sum(blockGABAdata,2);
+blockGABAdata = reshape(blockGABAdata, [npoints nblocks ]);
+
+GABAData = blockGABAdata';% back to [subspectra, points]
+
+%plot(real(GABAData(:,17000:18500))');
+
+for jj = 1:nblocks
     MRS_struct.pfile{ii};
     %%%%%%%%%%%% GABA fit (Gaussian) %%%%%%%%%%%%%%%
     % Originally (RE version)  gabadata was rescaled to the max across all spectra
@@ -57,9 +78,9 @@ for jj = 1:numsubspec
     %MRS_struct.freq(18000)
     %Hard code it to fit from 2.75 ppm to 3.55 ppm
     z=abs(MRS_struct.freq-3.55);
-    lowerbound=find(min(z)==z)
+    lowerbound=find(min(z)==z);
     z=abs(MRS_struct.freq-2.79);%2.75
-    upperbound=find(min(z)==z)
+    upperbound=find(min(z)==z);
     %lowerbound=17342;
     %upperbound=17961;
     %upperbound=18000;
@@ -136,33 +157,60 @@ for jj = 1:numsubspec
     GABAheight = GaussModelParam(jj,1);
     % FitSTD reports the standard deviation of the residuals / gaba HEIGHT
     %MRS_struct.GABAFitError(ii)  =  (ci(1,2)-ci(1,1))/sum(ci(1,:),2)*100;
-    MRS_struct.GABAFitError(ii)  =  100*std(residg)/GABAheight;
+    FitError(jj)  =  100*std(residg)/GABAheight;
     % x(2) = 1/(2*sigma^2).  Convert from points to Hz
 
     % area under gaussian is a * (2 * pi * sigma^2)^(1/2), and
     % GaussModelParam(:,2) = 1 / (2 * sigma^2)
     % This sets GabaArea as the area under the curve.
-    MRS_struct.gabaArea(ii)=GaussModelParam(ii,1)./sqrt(-GaussModelParam(ii,2))*sqrt(pi);
+    MRS_struct.GABA_area_subspec(ii,jj)=GaussModelParam(jj,1)./sqrt(-GaussModelParam(jj,2))*sqrt(pi);
 
-    sigma = ( 1 / (2 * (abs(GaussModelParam(ii,2)))) ).^(1/2);
-    MRS_struct.GABAFWHM(ii) =  abs( (2* 42.576*3) * sigma);
-    MRS_struct.GABAModelFit(ii,:)=GaussModelParam(ii,:);
 
-    figure(22)
-    plot(freq(freqbounds),GaussModel_area(GaussModelParam(jj,:),freq(freqbounds)),'r',...
-    freq(plotbounds),real(GABAData(jj,plotbounds)), 'b' )
-    %, ...
-    
-    %freq(freqbounds),residg,'b');
-    
-    legendtxt = regexprep(MRS_struct.pfile{ii}, '_','-');
-    title(legendtxt);
-    set(gca,'XDir','reverse');
-    %set(gca,'YTick',[], 'Xgrid', 'on');
-    oldaxis = axis;
-    axis( [2.6 3.6 oldaxis(3) oldaxis(4) ] )    
-    
-    end
+    %figure(22)
+    %plot(freq(freqbounds),GaussModel_area(GaussModelParam(jj,:),freq(freqbounds)),'r',...
+    %freq(plotbounds),real(GABAData(jj,plotbounds)), 'b' )
+
+%     legendtxt = regexprep(MRS_struct.pfile{ii}, '_','-');
+%     title(legendtxt);
+%     set(gca,'XDir','reverse');
+%     %set(gca,'YTick',[], 'Xgrid', 'on');
+%     oldaxis = axis;
+%     axis( [2.6 3.6 oldaxis(3) oldaxis(4) ] )
+
+end
+
+% work out all of the fits
+
+
+plotstart = 17000; plotend =17999;
+
+
+for jj=1:nblocks
+    % calculate fit, for plotting later
+    GABAblockfit(jj,:) = GaussModel_area(GaussModelParam(jj,:),freq(plotstart:plotend));
+end
+
+
+startoffset=0;
+offsetspec = mean(max(real(GABAData(1,plotstart:plotend))));
+
+endoffset = startoffset + ...
+    (nblocks-1) * offsetspec;
+offsetvals = [startoffset:offsetspec:endoffset]'
+
+offsetvals = repmat(offsetvals, [1 length([plotstart:plotend])]);
+size(offsetvals)
+size(GABAData(:,plotstart:plotend))
+
+plotdiffspec = real(GABAData(:,plotstart:plotend)) + offsetvals;
+GABAblockfit = GABAblockfit + offsetvals;
+
+figure(23)
+plot([plotstart:plotend],plotdiffspec','k', [plotstart:plotend], GABAblockfit, 'r')
+
+
+%plot(plotdiffspec')
+
 
 % end of MRSGABAfit
 
@@ -177,121 +225,6 @@ function F = GaussModel_area(x,freq)
 
 %F = x(1)*sqrt(-x(2)/pi)*exp(x(2)*(freq-x(3)).*(freq-x(3)))+x(4)*(freq-x(3))+x(5);
 F = x(1)*exp(x(2)*(freq-x(3)).*(freq-x(3)))+x(4)*(freq-x(3))+x(5);
-
-
-
-%%%%%%%%%%%%%%%%  OLD LORENTZGAUSSMODEL %%%%%%%%%%%%%%%%%%%%
-%function F = LorentzGaussModel(x,freq)
-%Lorentzian Model multiplied by a Gaussian.  gaussian width determined by
-%x(6). x(7) determines phase.
-%F = ((ones(size(freq))./(x(2)^2*(freq-x(3)).*(freq-x(3))+1)*x(1))*cos(x(7))+(ones(size(freq))./(x(2)^2*(freq-x(3)).*(freq-x(3))+1)*x(2).*(freq-x(3)))*sin(x(7))).*(exp(x(6)*(freq-x(3)).*(freq-x(3))))+x(4)*(freq-x(3))+x(5);
-
-
-%%%%%%%%%%%%%%%%  NEW LORENTZGAUSSMODEL %%%%%%%%%%%%%%%%%%%%
-function F = LorentzGaussModel(x,freq)
-% CJE 24Nov10 - removed phase term from fit - this is now dealt with
-% by the phasing of the water ref scans in MRSLoadPfiles
-%Lorentzian Model multiplied by a Gaussian.
-% x(1) = Amplitude of (scaled) Lorentzian
-% x(2) = 1 / hwhm of Lorentzian (hwhm = half width at half max)
-% x(3) = centre freq of Lorentzian
-% x(4) = constant baseline amplitude
-% x(5) =  -1 / 2 * sigma^2  of gaussian
-
-% 110624; remove linear baseline term
-% OBSOLETE:
-% x(4) = linear baseline amplitude
-% x(5) = constant baseline amplitude
-% x(6) =  -1 / 2 * sigma^2  of gaussian
-
-% Lorentzian  = (1/pi) * (hwhm) / (deltaf^2 + hwhm^2)
-% Peak height of Lorentzian = 4 / (pi*hwhm)
-% F is a normalised Lorentzian - height independent of hwhm
-%   = Lorentzian / Peak
-
-%F =((ones(size(freq))./(x(2)^2*(freq-x(3)).*(freq-x(3))+1)*x(1))*cos(x(7))+(ones(size(freq))./(x(2)^2*(freq-x(3)).*(freq-x(3))+1)*x(2).*(freq-x(3)))*sin(x(7))).*(exp(x(6)*(freq-x(3)).*(freq-x(3))))+x(4)*(freq-x(3))+x(5);
-% remove phasing
-F = (x(1)*ones(size(freq))./(x(2)^2*(freq-x(3)).*(freq-x(3))+1))  ...
-    .* (exp(x(6)*(freq-x(3)).*(freq-x(3)))) ... % gaussian
-    + x(4)*(freq-x(3)) ... % linear baseline
-    +x(5); % constant baseline
-
-% 110624 removed:
-% + x(4)*(freq-x(3)) ... % linear baseline
-% 111214 replaced:
-
-%%%%%%%%%%%%%%% BASELINE %%%%%%%%%%%%%%%%%%%%%%%
-function F = BaselineModel(x,freq)
-F = x(2)*(freq-x(1))+x(3);
-
-
-%%%%%%%%%%%%%%%%%%% INST UNITS CALC %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [MRS_struct] = MRSGABAinstunits(MRS_struct,ii)
-% function [MRS_struct] = MRSGABAinstunits(MRS_struct)
-% Convert GABA and Water amplitudes to institutional units
-% (pseudo-concentration in mmol per litre).
-% March 10: use MRS_struct.
-
-PureWaterConc = 55000; % mmol/litre
-WaterVisibility = 0.65; % This is approx the value from Ernst, Kreis, Ross
-EditingEfficiency = 0.5;
-T1_GABA = 0.80 ; % "empirically determined"...! Gives same values as RE's spreadsheet
-% ... and consistent with Cr-CH2 T1 of 0.8 (Traber, 2004)
-%Not yet putting in measured GABA T1, but it is in the pipeline - 1.35ish
-
-T2_GABA = 0.13; % from occipital Cr-CH2, Traber 2004
-T2_GABA = 0.088; % from JMRI paper 2011 Eden et al.
-
-T1_Water = 1.100; % average of WM and GM, estimated from Wansapura 1999
-T2_Water = 0.095; % average of WM and GM, estimated from Wansapura 1999
-SiteFactor=0.779; % site specific scaling factor, CUBRIC=0.779
-MM=0.45;  % MM correction: fraction of GABA in GABA+ peak. (In TrypDep, 30 subjects: 55% of GABA+ was MM)
-%This fraction is platform and implementation dependent, base on length and
-%shape of editing pulses and ifis Henry method. 
-%
-TR=1.8;
-TE=0.068;
-N_H_GABA=2;
-N_H_Water=2;
-Nspectra = length(MRS_struct.pfile);
-%Nwateravg=8;
-
-T1_factor = (1-exp(-TR./T1_Water)) ./ (1-exp(-TR./T1_GABA));
-T2_factor = exp(-TE./T2_Water) ./ exp(-TE./T2_GABA);
-
-MRS_struct.gabaiu(ii) = (MRS_struct.gabaArea(ii)  ./  MRS_struct.waterArea(ii))  ...
-    * PureWaterConc*WaterVisibility*T1_factor*T2_factor*(N_H_Water./N_H_GABA) ...
-    * MM * SiteFactor * (MRS_struct.Nwateravg ./ MRS_struct.Navg(ii)) ./ EditingEfficiency;
-
-%%%%%%%%%%%%%%% INSET FIGURE %%%%%%%%%%%%%%%%%%%%%%%
-function [h_main, h_inset]=inset(main_handle, inset_handle,inset_size)
-
-% The function plotting figure inside figure (main and inset) from 2 existing figures.
-% inset_size is the fraction of inset-figure size, default value is 0.35
-% The outputs are the axes-handles of both.
-%
-% An examle can found in the file: inset_example.m
-%
-% Moshe Lindner, August 2010 (C).
-
-if nargin==2
-    inset_size=0.35;
-end
-
-inset_size=inset_size*.5;
-%figure
-new_fig=gcf;
-main_fig = findobj(main_handle,'Type','axes');
-h_main = copyobj(main_fig,new_fig);
-set(h_main,'Position',get(main_fig,'Position'))
-inset_fig = findobj(inset_handle,'Type','axes');
-h_inset = copyobj(inset_fig,new_fig);
-ax=get(main_fig,'Position');
-set(h_inset,'Position', [1.3*ax(1)+ax(3)-inset_size 1.001*ax(2)+ax(4)-inset_size inset_size*0.7 inset_size*0.9])
-
-
-
-
 
 
 
